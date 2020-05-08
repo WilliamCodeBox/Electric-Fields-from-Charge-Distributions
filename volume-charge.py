5f823cf57def8ffc8e3b635ecc1dcf6ab0b8ed37
Liu Chang <luroylau@163.com>
2020-05-08 15:08:01 +0800

    sphere with uniform volume charge

---
+++ volume-charge.py
@@ -0,0 +1,5 @@
+""" A sphere of radius a centered at the origin with uniform volume charge density """
+
+# the resulting electric field at any point P(r, theta, phi)
+# is identical to the electirc field at the same point due to a point charge Q
+# located at the origin or the center of the spherical charge distribution
